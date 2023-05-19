c8aa97224b9b291db1483a6bbb1a5406d99e4843
daniel-akinyemi <danielakinyemi1999@gmail.com>
2023-05-19 17:05:45 +0100

    starter files updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import './App.css'
-import Donation from '../components/Donation'
-
 function App() {
 
   return (
     <div className="App">
-      <Donation/>
+      
       This is a text
     </div>
   )
