c2e47abeebf7037e9ee1ee7522cfa8fa8e891d03
daniel-akinyemi <danielakinyemi1999@gmail.com>
2023-05-27 13:03:29 +0100

    minor update

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
-
 const App = () => {
+
+  const planets = [
+    {name:'Mars', isGasPlanet:false},
+    {name:'Earth', isGasPlanet:false},
+    {name:'Jupiter', isGasPlanet:true},
+    {name:'Venus', isGasPlanet:false},
+    {name:'Neptune', isGasPlanet:true},
+    {name:'Uranus', isGasPlanet:true},
+  ];
   return (
-    <div>App</div>
+    <div>
+      {planets.map((planet,key) =>{
+        <div key={key}> {planet.isGasPlanet} && {planet.name}</div>
+      })}
+    </div>
   )
 }
 
