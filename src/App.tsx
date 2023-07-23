f029011c2ec9ea38c31e2b69a810fb93a78e6dd2
daniel-akinyemi <danielakinyemi1999@gmail.com>
2023-07-23 14:33:57 +0100

    api fetch (with axios) updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,36 @@
-const App = () => {
+import axios from "axios";
+import { useState, useEffect } from "react";
+import 'index.css'
 
+const App = () => {
+  const [fact,setFact] = useState()
   
+  useEffect(()=> {
+    const getCatFacts = async ()=>{
+      try{
+        const response = await axios.get('https://catfact.ninja/fact')
+        setFact(response.data)
+       }
+       catch(err){
+         console.error(err)
+       }
+    }
+    
+    getCatFacts()
+  },[])
+
   return (
     <div className="max-w-5xl mx-auto flex flex-col items-center">
       <h1 className="text-5xl">Generate An Excuse</h1>
       <div className="flex flex-col my-10 space-y-8 w-20">
-
-      <button className="bg-gray-500">Party</button>
-      <button className="button bg-gray-500">Family</button>
-      <button className="button bg-gray-500">Office</button>
+        <button className="bg-gray-500">Party</button>
+        <button className="button bg-gray-500">Family</button>
+        <button className="button bg-gray-500">Office</button>
       </div>
       <p></p>
+            
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
